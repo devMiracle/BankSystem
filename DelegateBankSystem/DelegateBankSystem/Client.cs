c70815f9de32c5debadbd84051ea448c7ca60b5a
devMiracle <rjcvjchzljv9@gmail.com>
2019-05-27 20:30:18 +0300

    add class Client

---
+++ DelegateBankSystem/DelegateBankSystem/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateBankSystem
+{
+    class Client
+    {
+        private string CardNumber { get; set; }
+        private string Password { get; set; }
+        private double ClientMoney { get; set; }
+
+    }
+}
---
+++ DelegateBankSystem/DelegateBankSystem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateBankSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
